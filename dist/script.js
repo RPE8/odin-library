3b253d16aef45a971ccbf978a169758acaaa6671
RPE8 <vitali.shuman96@gmail.com>
2022-09-04 15:52:46 +0300

    Add ids for checkboxes and labels' for attribute

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,14 @@
+function* numberGenrator(start = 0) {
+    let index = start;
+    while (true)
+        yield index++;
+}
+const generateId = () => {
+    return `id${idGenerator.next().value}`;
+};
 const booksList = document.getElementById("booksList");
 let books = [];
+const idGenerator = numberGenrator();
 const addBook = (books, book) => [...books, book];
 const createBookDisplayItem = (book) => {
     const title = document.createElement("h1");
@@ -12,11 +21,16 @@ const createBookDisplayItem = (book) => {
         books = books.filter(currBook => currBook !== book);
         updateBooksList();
     });
-    return [title, author, removeButton];
-};
-const createListItem = (childs) => {
+    const checkId = generateId();
+    const label = document.createElement("label");
+    label.textContent = "Read ";
+    label.setAttribute("for", checkId);
+    const readCheck = document.createElement("input");
+    readCheck.setAttribute("type", "checkbox");
+    readCheck.setAttribute("id", checkId);
+    readCheck.checked = book.read;
     const listItem = document.createElement("li");
-    listItem.append(...childs);
+    listItem.append(title, author, label, readCheck, removeButton);
     return listItem;
 };
 const displayBooks = (list, books) => {
@@ -25,7 +39,7 @@ const displayBooks = (list, books) => {
         return;
     }
     list.append(...books.reduce((items, book) => {
-        items.push(createListItem(createBookDisplayItem(book)));
+        items.push(createBookDisplayItem(book));
         return items;
     }, []));
 };
@@ -33,8 +47,8 @@ const updateBooksList = () => {
     booksList?.replaceChildren();
     displayBooks(booksList, books);
 };
-books = addBook(books, { title: "test", author: "test" });
-books = addBook(books, { title: "test2", author: "test2" });
-books = addBook(books, { title: "test3", author: "test3" });
+books = addBook(books, { title: "test", author: "test", read: false });
+books = addBook(books, { title: "test2", author: "test2", read: false });
+books = addBook(books, { title: "test3", author: "test3", read: false });
 displayBooks(booksList, books);
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,16 +1,33 @@
+
+
 type Book = {
 	title: string;
 	author: string;
+	read: boolean
 }
+
+type NumberGenerator = (start: number | undefined) => Generator<number>;
+type BookId = string;
+type IdGenerator = () => BookId;
 type BookAdder = (books: Book[], book: Book) => Book[]; 
 type BooksList = HTMLElement | null;
 type BooksDisplayer = (list: BooksList, books: Book[]) => void;
-type BookDisplayItemCreator = (book: Book) => Node[];
+type BookDisplayItemCreator = (book: Book) => HTMLLIElement;
 type ListItemCreator = (childs: Node[]) => HTMLLIElement; 
 
+function* numberGenrator(start = 0): Generator {
+	let index = start;
+  while (true)
+    yield index++;
+}
+
+const generateId: IdGenerator = () => {
+	return `id${idGenerator.next().value}`;
+}
 
 const booksList: BooksList = document.getElementById("booksList");
 let books: Book[] = [];
+const idGenerator = numberGenrator();
 
 const addBook: BookAdder = (books, book) => [...books, book];
 const createBookDisplayItem: BookDisplayItemCreator = (book) => {
@@ -24,16 +41,27 @@ const createBookDisplayItem: BookDisplayItemCreator = (book) => {
 		removeButton.addEventListener("click", (event) => {
 			books = books.filter(currBook => currBook !== book);
 			updateBooksList();
-		})
+		});
+		
+		const checkId = generateId();
 
-		return [title, author, removeButton];
-}
+		const label = document.createElement("label");
+		label.textContent = "Read ";
+		label.setAttribute("for", checkId);
 
-const createListItem:ListItemCreator = (childs) => {
-	const listItem = document.createElement("li");
-	listItem.append(...childs);
-	
-	return listItem;
+		
+
+		const readCheck = document.createElement("input");
+		readCheck.setAttribute("type", "checkbox");
+		readCheck.setAttribute("id", checkId);
+		readCheck.checked = book.read;
+
+		const listItem = document.createElement("li");
+		
+
+		listItem.append(title, author, label, readCheck, removeButton);
+
+		return listItem;
 }
 
 const displayBooks: BooksDisplayer = (list, books) => {
@@ -43,7 +71,7 @@ const displayBooks: BooksDisplayer = (list, books) => {
 	}
 
 	list.append(...books.reduce((items: HTMLLIElement[], book: Book): HTMLLIElement[] => {
-		items.push(createListItem(createBookDisplayItem(book)))
+		items.push(createBookDisplayItem(book))
 		return items;
 	}, []));
 }
@@ -53,8 +81,8 @@ const updateBooksList = () => {
 	displayBooks(booksList, books);
 }
 
-books = addBook(books, {title: "test", author: "test"});
-books = addBook(books, {title: "test2", author: "test2"});
-books = addBook(books, {title: "test3", author: "test3"});
+books = addBook(books, {title: "test", author: "test", read: false});
+books = addBook(books, {title: "test2", author: "test2", read: false});
+books = addBook(books, {title: "test3", author: "test3", read: false});
 displayBooks(booksList, books);
 
