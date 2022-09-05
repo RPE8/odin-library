6e0b45822373f996ab77ff5d654ba19b7e7e54e5
RPE8 <vitali.shuman96@gmail.com>
2022-09-05 20:22:22 +0300

    Rework 'remove' functionality

--- dist/script.js
+++ dist/script.js
@@ -9,6 +9,11 @@ const generateId = () => {
 const booksList = document.getElementById("booksList");
 let books = [];
 const idGenerator = numberGenrator();
+const removeBook = (books, book2Delete) => books.filter((book) => {
+    if (typeof book2Delete === "string")
+        return book2Delete !== book.id;
+    return book2Delete !== book;
+});
 const addBook = (books, book) => [...books, book];
 const createBookDisplayItem = (book) => {
     const template = document.createElement("template");
@@ -16,11 +21,27 @@ const createBookDisplayItem = (book) => {
 			<h1 class="books-section__title title">${book.title}</h1>
 			<h2 class="books-section__author author">${book.author}</h2>
 			<div class="books-section__buttons-container">
-				<button class="books-section__read-button book-button ${book.read ? "read" : "unread"}">Read</button>
+				<button class="books-section__read-button book-button book-button--${book.read ? "read" : "unread"}">${book.read ? "Read" : "Not Read"}</button>
 				<button class="books-section__remove-button book-button">Remove</button>
 			</div>
 		</li>`;
     return template.content.firstChild;
+};
+const handleBookRemove = (event) => {
+    const target = event.target;
+    const currentTarget = event.currentTarget;
+    const classList = target.classList;
+    if (target.classList.contains("books-section__remove-button")) {
+        if (!currentTarget.dataset.id)
+            return;
+        books = removeBook(books, currentTarget.dataset.id);
+        updateBooksList();
+        return;
+    }
+    if (target.classList.contains("books-section__read-button")) {
+        console.log(target);
+        return;
+    }
 };
 const displayBooks = (list, books) => {
     if (!list || !Array.isArray(books)) {
@@ -28,7 +49,9 @@ const displayBooks = (list, books) => {
         return;
     }
     list.append(...books.reduce((items, book) => {
-        items.push(createBookDisplayItem(book));
+        const listItem = createBookDisplayItem(book);
+        listItem.addEventListener("click", handleBookRemove);
+        items.push(listItem);
         return items;
     }, []));
 };
@@ -38,6 +61,6 @@ const updateBooksList = () => {
 };
 books = addBook(books, { title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nobis, sit sapiente quaerat doloribus ipsum odio tempore rem sint deserunt.", author: "test", read: false, id: generateId() });
 books = addBook(books, { title: "test2", author: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, sequi.", read: false, id: generateId() });
-books = addBook(books, { title: "Lorem ipsum dolor sit.", author: "test3", read: false, id: generateId() });
+books = addBook(books, { title: "Lorem ipsum dolor sit.", author: "test3", read: true, id: generateId() });
 displayBooks(booksList, books);
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -10,7 +10,8 @@ type Book = {
 type NumberGenerator = (start: number | undefined) => Generator<number>;
 type BookId = string;
 type IdGenerator = () => BookId;
-type BookAdder = (books: Book[], book: Book) => Book[]; 
+type BookAdder = (books: Book[], book: Book) => Book[];
+type BookRemover = (books: Book[], book: Book | BookId) => Book[];
 type BooksList = HTMLElement | null;
 type BooksDisplayer = (list: BooksList, books: Book[]) => void;
 type BookDisplayItemCreator = (book: Book) => HTMLLIElement;
@@ -30,6 +31,12 @@ const booksList: BooksList = document.getElementById("booksList");
 let books: Book[] = [];
 const idGenerator = numberGenrator();
 
+const removeBook: BookRemover = (books, book2Delete) => books.filter((book) => {
+	if (typeof book2Delete === "string") return book2Delete !== book.id;
+
+	return book2Delete !== book;
+});
+
 const addBook: BookAdder = (books, book) => [...books, book];
 const createBookDisplayItem: BookDisplayItemCreator = (book) => {
 		const template = document.createElement("template");
@@ -37,12 +44,28 @@ const createBookDisplayItem: BookDisplayItemCreator = (book) => {
 			<h1 class="books-section__title title">${book.title}</h1>
 			<h2 class="books-section__author author">${book.author}</h2>
 			<div class="books-section__buttons-container">
-				<button class="books-section__read-button book-button ${book.read ? "read" : "unread"}">Read</button>
+				<button class="books-section__read-button book-button book-button--${book.read ? "read" : "unread"}">${book.read ? "Read" : "Not Read"}</button>
 				<button class="books-section__remove-button book-button">Remove</button>
 			</div>
 		</li>`
 
 		return template.content.firstChild as HTMLLIElement;
+}
+
+const handleBookRemove = (event: Event): void => {
+	const target = event.target as HTMLElement;
+	const currentTarget = event.currentTarget as HTMLElement;
+	const classList = target.classList;
+	if (target.classList.contains("books-section__remove-button")) {
+		if (!currentTarget.dataset.id) return;
+		books = removeBook(books, currentTarget.dataset.id);
+		updateBooksList();
+		return;
+	}
+	if (target.classList.contains("books-section__read-button")) {
+		console.log(target)
+		return;
+	}
 }
 
 const displayBooks: BooksDisplayer = (list, books) => {
@@ -51,8 +74,11 @@ const displayBooks: BooksDisplayer = (list, books) => {
 		return;
 	}
 
+
 	list.append(...books.reduce((items: HTMLLIElement[], book: Book): HTMLLIElement[] => {
-		items.push(createBookDisplayItem(book))
+		const listItem = createBookDisplayItem(book);
+		listItem.addEventListener("click", handleBookRemove)
+		items.push(listItem);
 		return items;
 	}, []));
 }
@@ -64,6 +90,6 @@ const updateBooksList = () => {
 
 books = addBook(books, {title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nobis, sit sapiente quaerat doloribus ipsum odio tempore rem sint deserunt.", author: "test", read: false, id: generateId()});
 books = addBook(books, {title: "test2", author: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, sequi.", read: false, id: generateId()});
-books = addBook(books, {title: "Lorem ipsum dolor sit.", author: "test3", read: false, id: generateId()});
+books = addBook(books, {title: "Lorem ipsum dolor sit.", author: "test3", read: true, id: generateId()});
 displayBooks(booksList, books);
 
